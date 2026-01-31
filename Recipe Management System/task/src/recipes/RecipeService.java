32098d5ea40fd0757f6603ef70fc96447d95d8de
Beata Kaźmierczak <beatakazmierczak@MacBook-Pro-Beata-2.local>
2026-01-31 12:32:01 +0100

    Clean code after stage 3

--- Recipe Management System/task/src/recipes/RecipeService.java
+++ Recipe Management System/task/src/recipes/RecipeService.java
@@ -13,36 +13,10 @@ import java.util.List;
 @AllArgsConstructor
 @Slf4j
 public class RecipeService {
+
     private RecipeRepository recipeRepository;
 
-
-//    Identifier addRecipe(String recipe) {
-//        if (StringUtils.isNotBlank(recipe)) {
-//            log.error("1");
-//            throw new IllegalArgumentException("No recipe");
-//        }
-//        final Gson gson = new Gson();
-//        final Recipe recipeEntity = gson.fromJson(recipe, Recipe.class);
-//        if (recipeEntity == null) {
-//            log.error("2");
-//            throw new IllegalArgumentException("Couldn't create recipe entity");
-//        }
-//        if (StringUtils.isNotBlank(recipeEntity.getName()) || StringUtils.isNotBlank(recipeEntity.getDescription())) {
-//            log.error("3");
-//            throw new IllegalArgumentException("Invalid recipe field. Recipe: %s.".formatted(recipeEntity));
-//        }
-//        if (CollectionUtils.isEmpty(recipeEntity.getIngredients()) || CollectionUtils.isEmpty(recipeEntity.getDirections())) {
-//            log.error("4");
-//            throw new IllegalArgumentException("No full instructions!");
-//        }
-//        final Recipe savedRecipe = recipeRepository.save(recipeEntity);
-//        return new Identifier(savedRecipe.getId());
-//
-//    }
-
     Identifier addRecipe(Recipe recipe) {
-     //   final Gson gson = new Gson();
-    //    final Recipe recipeEntity = gson.fromJson(recipe, Recipe.class);
         final Recipe savedRecipe = recipeRepository.save(recipe);
         return new Identifier(savedRecipe.getId());
     }
